90f731152cb1a8069ed46a7f76a36da42723d5a9
Khalid Alabi Dean <alabikhalid.12@gmail.com>
2025-02-06 03:51:30 +0100

    Update settings.py

--- stage1/settings.py
+++ stage1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-1miz^r1okg2o(&wzwxug7)kj32&sn!vv_n%70c0y#th=bvj&j9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
