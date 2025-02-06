a6f4eec313fb7d924637bddec6b1d489cca79e5e
Alabi Khalid <alabikhalid.12@gmail.com>
2025-02-06 04:28:40 +0100

    added cors header to project

--- stage1/settings.py
+++ stage1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-1miz^r1okg2o(&wzwxug7)kj32&sn!vv_n%70c0y#th=bvj&j9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -123,3 +124,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CORS_ORIGIN_ALLOW_ALL = True
